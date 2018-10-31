544af5280acb69b4e35f754d12c75ddbb1931dac
Harlan Wilton <harlan103@hotmail.com>
2018-10-31 23:45:09 +0000

    Apply fixes from StyleCI

--- src/Api.php
+++ src/Api.php
@@ -2,15 +2,14 @@
 
 namespace Loonpwn\Swiftype;
 
-
-use Loonpwn\Swiftype\Errors\MissingSwiftypeConfigException;
 use Loonpwn\Swiftype\Facades\SwiftypeEngine;
+use Loonpwn\Swiftype\Errors\MissingSwiftypeConfigException;
 
 class Api
 {
     private const REQUIRED_CONFIG = [
         'SWIFTYPE_API_PRIVATE_KEY' => 'apiPrivateKey',
-        'SWIFTYPE_HOST_IDENTIFIER' => 'hostIdentifier'
+        'SWIFTYPE_HOST_IDENTIFIER' => 'hostIdentifier',
     ];
 
     private $client;
@@ -22,71 +21,79 @@ class Api
      *
      * @throws MissingSwiftypeConfigException
      */
-    public function __construct() {
+    public function __construct()
+    {
         $swiftypeConfig = config('swiftype');
         // check the environment configs that we need have been set
-        foreach(self::REQUIRED_CONFIG as $env => $value) {
+        foreach (self::REQUIRED_CONFIG as $env => $value) {
             if (empty($swiftypeConfig[$value])) {
-                throw new MissingSwiftypeConfigException( $env);
+                throw new MissingSwiftypeConfigException($env);
             }
         }
 
         $config = array_merge(config('swiftype'), [
-            'apiUrl' => 'https://' . config('swiftype.hostIdentifier') . '.api.swiftype.com/api/as/v1/'
+            'apiUrl' => 'https://'.config('swiftype.hostIdentifier').'.api.swiftype.com/api/as/v1/',
         ]);
         $this->client = new SwiftypeClient($config);
     }
 
-	/**
-	 * Get the engine instance based on the engine name
-	 * @param null $engine
-	 *
-	 * @return SwiftypeEngine
-	 */
-    public function engine($engine = null) {
+    /**
+     * Get the engine instance based on the engine name.
+     * @param null $engine
+     *
+     * @return SwiftypeEngine
+     */
+    public function engine($engine = null)
+    {
         return new SwiftypeEngine($engine);
     }
 
     /**
-     * Retrieve an array of engines
+     * Retrieve an array of engines.
      *
      * @return array An array of the engines
      */
-    public function listEngines() {
-        $params = [ 'per_page' => 100 ];
-        $response = $this->client->get('engines' , ['query' => $params] );
-        return json_decode( $response->getBody()->getContents(), true );
+    public function listEngines()
+    {
+        $params = ['per_page' => 100];
+        $response = $this->client->get('engines', ['query' => $params]);
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 
     /**
-     * Retrieve a specific engine
+     * Retrieve a specific engine.
      *
      * @param string $name The name of the engine to be found
      * @return array The engine that was found
      */
-    public function findEngine($name) {
-        $response = $this->client->get('engines/' . $name);
-        return json_decode( $response->getBody()->getContents(), true );
+    public function findEngine($name)
+    {
+        $response = $this->client->get('engines/'.$name);
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 
     /**
-     * Create an engine
+     * Create an engine.
      *
      * @param array $name The name of the engine to create
      * @return array The engine that was created
      */
-    public function createEngine($name) {
-        $engine = [ 'name' => $name ];
+    public function createEngine($name)
+    {
+        $engine = ['name' => $name];
         $response = $this->client->post('engines', $engine);
-        return json_decode( $response->getBody()->getContents(), true );
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 
     /**
-     * Check whether the connection could be authenticated
+     * Check whether the connection could be authenticated.
      * @return bool
      */
-    public function authenticated() {
-        return !empty($this->listEngines());
+    public function authenticated()
+    {
+        return ! empty($this->listEngines());
     }
-
 }
--- src/Errors/MissingSwiftypeConfigException.php
+++ src/Errors/MissingSwiftypeConfigException.php
@@ -1,19 +1,19 @@
 <?php
+
 namespace Loonpwn\Swiftype\Errors;
 
 use Exception;
 
-class MissingSwiftypeConfigException extends Exception {
-
-
+class MissingSwiftypeConfigException extends Exception
+{
     /**
      * MissingSwiftypeConfigException constructor.
      *
      * @param string $key
      */
-    public function __construct(string $key) {
-        $this->message = 'Missing Config Exception! Add the ' . $key . ' to your .env file.';
+    public function __construct(string $key)
+    {
+        $this->message = 'Missing Config Exception! Add the '.$key.' to your .env file.';
         $this->code = 500;
     }
-
 }
